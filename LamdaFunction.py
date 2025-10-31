1ef80713517d06ba5bbec9034c22b2f9205d7a09
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-10-31 13:46:00 +0530

    Lamda Function

--- LamdaFunction.py
+++ LamdaFunction.py
@@ -15,9 +15,35 @@
 
 
 
-#sum of digit ]
+#sum 1 to n
 #using the formula
 #sum = n*(n+1)/2
 
-sumOfDigit = lambda n:(n*(n+1))//2
-print(sumOfDigit(10))
+# sumOfDigit = lambda n:(n*(n+1))//2
+# print(sumOfDigit(10))
+# sum of digit 
+
+
+# digit = 91
+# sum=0
+
+# digitSum = lambda x,t:x+t
+
+# while(digit>0):
+#     temp=digit%10
+#     sum = digitSum(sum,temp)
+#     digit//=10
+
+# print(sum)
+
+
+
+
+#sum of digit using ruduce function and lamda function 
+from functools import reduce
+num=1234
+
+digits = list(map(int,str(num)))
+sumOfDigit=reduce(lambda x,y:x+y,digits)
+
+print(sumOfDigit)
