b1e725bac01885f03c8b0496f798fdffc4a9bf94
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-10-31 12:47:48 +0530

    Learning Lamda Function

---
+++ LamdaFunction.py
@@ -0,0 +1,15 @@
+# lambda function in Python is:
+# ➡️ A small anonymous function (no name)(also know as ).
+# ➡️ Can take any number of arguments but has only one expression.
+# ➡️ The expression is automatically returned (no need for return).
+#lambda arguments: expression
+# add = lambda x, y: x + y
+# print(add(5, 3))  # Output: 8
+
+
+# multiply =lambda x,y :x*y
+# print(multiply(5,3))
+
+add = lambda a,b,c: (a+b)*c
+
+print(add(2,4,2))
