e8faad919886f8be724b56d3c13a38999eb9bcfc
Ritikyadav2004 <ritikyadav5000@gmail.com>
2025-11-04 09:52:18 +0530

    Packages

---
+++ MyMath.py
@@ -0,0 +1,23 @@
+x=888
+def add(a,b):
+    return a+b
+
+def pdt(a,b):
+    return a*b
+
+def divide(a,b):
+    if(b!=0):
+        return a/b
+    else:
+        return "Cannot divide with 0"
+    
+
+sumOfnTerms=lambda n:(n*(n+1))//2
+
+
+
+
+def square(x):
+    
+    return x**2;
+
